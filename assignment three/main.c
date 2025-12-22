8f338b060de8a4c57c197e30c862a97234d37a32
marcusunbound <marcothuo274@gmail.com>
2025-12-22 22:27:02 +0300

    Implement pointer operations and function examples
    Added functions to swap numbers and increment values by reference and value. Updated main function to demonstrate pointer usage and arithmetic operations.

--- assignment three/main.c
+++ assignment three/main.c
@@ -1,6 +1,78 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+void swapNumbers(int* num1, int* num2)
+{
+
+    int numOld1 = *num1;
+    int numOld2 = *num2;
+    *num2 = numOld1;
+    *num1 = numOld2;
+}
+
+void incrementByValue(int num1)
+{
+    num1 += 30;
+}
+
+void incrementByReference(int* num1)
+{
+    *num1 += 30;
+}
+
+int main(void)
+{
+    //Display address of a variable
+    int num = 30;
+    int* ptr = &num;
+
+    printf("value stored in pointer: %d\n", *ptr);
+    printf("Address of num value %p\n", ptr);
+
+
+    //access variable value using pointer
+    int count = 300;
+    int* pCount;
+    pCount = &count;
+
+    *pCount = 60;
+    printf("count: %d pCount: %d\n\n", count, *pCount);
+
+
+    //add two numbers using pointers
+    int num1, num2;
+    int*  ptr1;
+    int* ptr2;
+    num1 = 30;
+    num2 = 60;
+
+    ptr1 = &num1;
+    ptr2 = &num2;
+
+    int addResult = *ptr1 + *ptr2;
+    printf("Addition result: %d\n\n", addResult);
+
+
+    //swap two numbers using pointers
+    int a, b;
+    a = 30;
+    b = 31;
+
+    int *aPtr = &a;
+    int *bPtr = &b;
+
+    printf("old values before swapping: num1: %d, num2: %d\n", *aPtr, *bPtr);
+    swapNumbers(aPtr, bPtr);
+    printf("new values after swapping: num1: %d, num2: %d\n\n", *aPtr, *bPtr);
+
+
+    //Pass by value and pass by reference
+    int value = 120;
+    incrementByValue(value);
+    printf("increment by value result: %d\n", value);
+
+    incrementByReference(&value);
+    printf("increment by reference result: %d\n", value);
+
+
     return 0;
 }
